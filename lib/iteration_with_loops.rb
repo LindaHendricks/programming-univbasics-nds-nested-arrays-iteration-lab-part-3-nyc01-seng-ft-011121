ef3c5cc89c4ca36228ae831ab1ce33662846ed98
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-31 17:16:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0 
     new_string = " "
     
-    while element_index < src[row_index].count do 
+    while element_index > src[row_index].count do 
       if src[row_index][element_index].class == String
        new_string << src[row_index][element_index]
        
